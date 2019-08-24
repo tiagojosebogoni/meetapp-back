f7fe6932d5c50a5efef40942f2d78c43c1df4d7b
tiago <tiagojosebogoni@gmail.com>
2019-08-24 00:17:15 -0300

    Adicionado funçao para salvar usuario

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,18 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existente' });
+    }
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+}
+
+export default new UserController();
