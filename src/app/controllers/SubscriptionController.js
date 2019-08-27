a1c401193ae5b24035586a79000a9dee534b2e77
tiago <tiagojosebogoni@gmail.com>
2019-08-26 22:46:11 -0300

    Adicionado verificaçao para nao deixar se inscrever no mesmo meetup 2 vezes

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,3 +1,61 @@
-class SubscriptionController {}
+import Meetup from '../models/Meetup';
+import Subscription from '../models/Subscription';
+
+class SubscriptionController {
+  async index(req, res) {
+    // const meetups = await Meetup.find;
+    return res.json('meetups');
+  }
+
+  async store(req, res) {
+    const user_id = req.userId;
+    const meetup_id = req.params.meetupId;
+    const meetup = await Meetup.findByPk(meetup_id);
+
+    /**
+     * O usuário deve poder se inscrever em meetups que não organiza.
+     */
+    if (meetup.user_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'Não é possível se inscrever no mesmo meetup' });
+    }
+
+    /**
+     * O usuário não pode se inscrever em meetups que já aconteceram.
+     */
+    if (meetup.past) {
+      return res.json({
+        error: 'Não é possível se inscrever em meetup passados',
+      });
+    }
+
+    /**
+     * O usuário não pode se inscrever no mesmo meetup duas vezes.
+     */
+    const registrations = await Subscription.findAll({
+      where: { meetup_id, user_id },
+    });
+
+    const alreadySubscribed = registrations.length > 0;
+
+    if (alreadySubscribed) {
+      return res
+        .status(400)
+        .json({ error: 'Você já está inscrito neste Meetup' });
+    }
+
+    /**
+     * O usuário não pode se inscrever em dois meetups que acontecem no mesmo horário.
+     */
+
+    const subscription = await Subscription.create({
+      user_id,
+      meetup_id,
+    });
+
+    return res.json(subscription);
+  }
+}
 
 export default new SubscriptionController();
--- src/routes.js
+++ src/routes.js
@@ -7,19 +7,23 @@ import authMiddleware from './app/middleware/auth';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/user', UserController.store);
+routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/user', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
 
 export default routes;
