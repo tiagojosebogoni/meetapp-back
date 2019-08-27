11605f114ab0e3922841f1bff259e9bc568dc7fe
tiago <tiagojosebogoni@gmail.com>
2019-08-27 14:50:33 -0300

    Adicionado funçao para mandar email depois de uma nova inscriçao

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd2b47af2ce35f8',
+    pass: '5a1dc5efe16a72',
+  },
+  default: {
+    from: 'Organização Meetup LTDA <organizacao@meetup.com.br>',
+  },
+};
