93b530ae9fd06ac98bc194b4980c0b4889e94f1e
tiago <tiagojosebogoni@gmail.com>
2019-08-27 14:50:18 -0300

    Adicionado funçao para mandar email depois de uma nova inscriçao

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,5 +1,10 @@
+import pt from 'date-fns/locale/pt';
+import { fn, col } from 'sequelize';
+import { format } from 'date-fns';
 import Meetup from '../models/Meetup';
 import Subscription from '../models/Subscription';
+import User from '../models/User';
+import Mail from '../../lib/Mail';
 
 class SubscriptionController {
   async index(req, res) {
@@ -10,7 +15,14 @@ class SubscriptionController {
   async store(req, res) {
     const user_id = req.userId;
     const meetup_id = req.params.meetupId;
-    const meetup = await Meetup.findByPk(meetup_id);
+    const meetup = await Meetup.findByPk(meetup_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     /**
      * O usuário deve poder se inscrever em meetups que não organiza.
@@ -48,13 +60,56 @@ class SubscriptionController {
     /**
      * O usuário não pode se inscrever em dois meetups que acontecem no mesmo horário.
      */
+    const checkDate = await Subscription.findOne({
+      where: { user_id },
+      include: [
+        {
+          model: Meetup,
+          where: { date: meetup.date },
+        },
+      ],
+    });
 
-    const subscription = await Subscription.create({
+    if (checkDate) {
+      return res.status(400).json({
+        error: `Você já está inscrito no evento de ${checkDate.Meetup.title}, neste horário`,
+      });
+    }
+
+    /* const subscription = await Subscription.create({
       user_id,
       meetup_id,
     });
+*/
+    /**
+     * Sempre que um usuário se inscrever no meetup, envie um e-mail ao organizador contendo os dados relacionados ao usuário inscrito.
+     */
+    const formattedDate = format(
+      meetup.date,
+      "'no' 'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+    const user = await User.findByPk(user_id);
 
-    return res.json(subscription);
+    const count = await Subscription.count({
+      where: { meetup_id },
+      attributes: [[fn('count', col('meetup_id')), 'meetup']],
+    });
+
+    await Mail.sendMail({
+      to: `${meetup.User.name} <${meetup.User.email}>`,
+      subject: `Nova Inscrição no Meetup de ${meetup.title}, ${formattedDate}`,
+      // text: `Uma nova inscrição de ${user.name}`,
+      template: 'send',
+      context: {
+        speaker: meetup.User.name,
+        user: user.name,
+        count,
+        img: 'meetapp.svg',
+      },
+    });
+
+    return res.json('subscription');
   }
 }
 
